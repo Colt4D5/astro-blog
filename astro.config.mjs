0cb7c1c2b945d1df8e159d9603e7acfb8fd90404
Colton <allen.colton@hotmail.com>
2023-09-17 15:41:09 -0700

    reconfigured to static routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
-import nodejs from '@astrojs/node';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
   integrations: [react(), svelte()]
 });
